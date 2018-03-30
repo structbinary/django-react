0164153632fcd2a2098d10e8e1caa226d1559e21
structbinary <sandeepmahto4@gmail.com>
2018-03-30 11:56:22 +0530

    integrated react with django

--- devOpsDash/devOpsDash/settings.py
+++ devOpsDash/devOpsDash/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,10 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'devOpsDash.urls'
-
+CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- devOpsDash/frontend/src/App.js
+++ devOpsDash/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -8,14 +9,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello Zukin</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hope you are good today, To start working edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" onClick={this.onClick }> send get /calls </button>
       </div>
     );
   }
+  onClick(ev) {
+    console.log("Sending a GET API Call !!!");
+    axios.get('http://127.0.0.1:8000/products').then(res => {
+      console.log(res.json())
+    }).then(response => {
+      console.log(JSON.stringify(response));
+    } )
+}
 }
 
 export default App;
